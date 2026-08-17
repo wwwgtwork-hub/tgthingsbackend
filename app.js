e782e7820947590a83605383eecfc18b6abd083b
wwwgtwork-hub <www.gtwork@gmail.com>
2026-08-17 13:51:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const cors = require("cors");
 const app = express();
 const router = require("./routs/rout.js");
 const db = require("./db");
-app.use(cors());
+app.use(cors(
+  origin: ["https://tgthings3444.web.app"]
+));
 app.use(express.json());
 app.use("/api", router);
 const PORT = process.env.PORT || 5000;
